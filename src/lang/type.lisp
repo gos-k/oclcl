a7796cb9b15340788b35bdb282fa8281c002bcee
gos-k <mag4.elan@gmail.com>
2015-12-01 06:34:13 +0900

    add vector types

--- src/lang/type.lisp
+++ src/lang/type.lisp
@@ -20,10 +20,67 @@
            :ulong
            :float
            :double
+
+           :char2
+           :char3
+           :char4
+           :char8
+           :char16
+
+           :uchar2
+           :uchar3
+           :UCHAR4
+           :uchar8
+           :uchar16
+
+           :short2
+           :short3
+           :short4
+           :short8
+           :short16
+
+           :ushort2
+           :ushort3
+           :ushort4
+           :ushort8
+           :ushort16
+
+           :int2
+           :int3
+           :int4
+           :int8
+           :int16
+
+           :uint2
+           :uint3
+           :uint4
+           :uint8
+           :uint16
+
+           :long2
+           :long3
+           :long4
+           :long8
+           :long16
+
+           :ulong2
+           :ulong3
+           :ulong4
+           :ulong8
+           :ulong16
+
+           :float2
            :float3
            :float4
+           :float8
+           :float16
+
+           :double2
            :double3
            :double4
+           :double8
+           :double16
+
            :bool*
            :int*
            :float*
