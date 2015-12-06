1051b8b7d42394a24d627aa19df8a19e291bbc36
gos-k <mag4.elan@gmail.com>
2015-12-06 20:20:41 +0900

    remove unused files

--- misc/package.lisp
+++
@@ -1,9 +0,0 @@
-#|
-  This file is a part of cl-cuda project.
-  Copyright (c) 2012 Masayuki Takagi (kamonama@gmail.com)
-|#
-
-(in-package :cl-user)
-(defpackage cl-cuda-misc
-  (:use :cl)
-  (:export :convert-error-string))
