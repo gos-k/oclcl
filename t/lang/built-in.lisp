5a6ce62be086d6a34421f04d446ca4d84c5803b3
gos-k <mag4.elan@gmail.com>
2015-12-20 03:25:05 +0900

    modify built-in unittest

--- t/lang/built-in.lisp
+++ t/lang/built-in.lisp
@@ -17,58 +17,40 @@
 ;;; test BUILT-IN-FUNCTION-RETURN-TYPE function
 ;;;
 
-(diag "BUILT-IN-FUNCTION-RETURN-TYPE")
-
-(dolist (operator '(+ * - / mod))
-  (is (built-in-function-return-type operator '(int int)) 'int))
-(dolist (operator '(+ * - /))
-  (is (built-in-function-return-type operator '(float float)) 'float))
-(dolist (operator '(+ * - / mod))
-  (is (built-in-function-return-type operator '(int4 int4)) 'int4))
-(dolist (operator '(+ * - /))
-  (is (built-in-function-return-type operator '(float4 float4)) 'float4))
-(dolist (operator '(< > <= >=))
-  (is (built-in-function-return-type operator '(int int)) 'int))
-(dolist (operator '(< > <= >=))
-  (is (built-in-function-return-type operator '(float4 float4)) 'int4))
-(dolist (operator '(< > <= >=))
-  (is (built-in-function-return-type operator '(double4 double4)) 'long4))
+(subtest "BUILT-IN-FUNCTION-RETURN-TYPE"
+  (dolist (operator '(+ * - / mod))
+    (is (built-in-function-return-type operator '(int int)) 'int))
+  (dolist (operator '(+ * - /))
+    (is (built-in-function-return-type operator '(float float)) 'float))
+  (dolist (operator '(+ * - / mod))
+    (is (built-in-function-return-type operator '(int4 int4)) 'int4))
+  (dolist (operator '(+ * - /))
+    (is (built-in-function-return-type operator '(float4 float4)) 'float4))
+  (dolist (operator '(< > <= >=))
+    (is (built-in-function-return-type operator '(int int)) 'int))
+  (dolist (operator '(< > <= >=))
+    (is (built-in-function-return-type operator '(float4 float4)) 'int4))
+  (dolist (operator '(< > <= >=))
+    (is (built-in-function-return-type operator '(double4 double4)) 'long4)))
 
 ;;;
 ;;; test BUILT-IN-FUNCTION-INFIX-P function
 ;;;
 
-(diag "BUILT-IN-FUNCTION-INFIX-P")
-
-(is (built-in-function-infix-p '+ '(int int)) t
-    "basic case 1")
-
-(is (built-in-function-infix-p '+ '(float3 float3)) t
-    "basic case 2")
-
-(is (built-in-function-infix-p '- '(int int)) t
-    "basic case 3")
-
-(is (built-in-function-infix-p 'mod '(int int)) t
-    "basic case 4")
+(subtest "BUILT-IN-FUNCTION-INFIX-P"
+  (ok (built-in-function-infix-p '+ '(int int)))
+  (ok (built-in-function-infix-p '+ '(float3 float3)))
+  (ok (built-in-function-infix-p '- '(int int)))
+  (ok (built-in-function-infix-p 'mod '(int int))))
 
 ;;;
 ;;; test BUILT-IN-FUNCTION-C-NAME function
 ;;;
 
-(diag "BUILT-IN-FUNCTION-C-NAME")
-
-(is (built-in-function-c-name '+ '(int int)) "+"
-    "basic case 1")
-
-(is (built-in-function-c-name '+ '(float3 float3)) "+"
-    "basic case 2")
-
-(is (built-in-function-c-name '- '(int int)) "-"
-    "basic case 3")
-
-(is (built-in-function-c-name 'mod '(int int)) "%"
-    "basic case 4")
-
+(subtest "BUILT-IN-FUNCTION-C-NAME"
+  (is (built-in-function-c-name '+ '(int int)) "+")
+  (is (built-in-function-c-name '+ '(float3 float3)) "+")
+  (is (built-in-function-c-name '- '(int int)) "-")
+  (is (built-in-function-c-name 'mod '(int int)) "%"))
 
 (finalize)
