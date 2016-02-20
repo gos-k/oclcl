4e28f002473a803a8a80469b855cca62cb847390
gos-k <mag4.elan@gmail.com>
2016-02-20 21:06:12 +0900

    modify unittest util.lisp to subtest

--- t/lang/util.lisp
+++ t/lang/util.lisp
@@ -17,62 +17,57 @@
 ;;; test C-IDENTIFIER function
 ;;;
 
-(diag "C-IDENTIFIER")
-
-(is (c-identifier 'x) "x"
-    "basic case 1")
-(is (c-identifier 'vec-add-kernel) "vec_add_kernel"
-    "basic case 2")
-(is (c-identifier 'vec.add.kernel) "vec_add_kernel"
-    "basic case 3")
-(is (c-identifier '%vec-add-kernel) "_vec_add_kernel"
-    "basic case 4")
-(is (c-identifier 'VecAdd_kernel) "vecadd_kernel"
-    "basic case 5")
-(is (c-identifier 'foo t) "oclcl_test_lang_util_foo"
-    "basic case 6")
+(subtest "C-IDENTIFIER"
+  (is (c-identifier 'x) "x"
+      "basic case 1")
+  (is (c-identifier 'vec-add-kernel) "vec_add_kernel"
+      "basic case 2")
+  (is (c-identifier 'vec.add.kernel) "vec_add_kernel"
+      "basic case 3")
+  (is (c-identifier '%vec-add-kernel) "_vec_add_kernel"
+      "basic case 4")
+  (is (c-identifier 'VecAdd_kernel) "vecadd_kernel"
+      "basic case 5")
+  (is (c-identifier 'foo t) "oclcl_test_lang_util_foo"
+      "basic case 6"))
 
 
 ;;;
 ;;; test LINES function
 ;;;
 
-(diag "LINES")
-
-(is (lines (format nil "1~%2~%3~%")) '("1" "2" "3")
-    "basic case 1")
-
-(is (lines (format nil "1~%2~%3")) '("1" "2" "3")
-    "basic case 2")
+(subtest "LINES"
+  (is (lines (format nil "1~%2~%3~%")) '("1" "2" "3")
+      "basic case 1")
+  (is (lines (format nil "1~%2~%3")) '("1" "2" "3")
+      "basic case 2"))
 
 
-;;;
+;
 ;;; test UNLINES function
 ;;;
 
-(diag "UNLINES")
-
-(is (unlines "1" "2" "3") "1
+(subtest "UNLINES"
+  (is (unlines "1" "2" "3") "1
 2
 3
-" "basic case 1")
+" "basic case 1"))
 
 
 ;;;
 ;;; test INDENT function
 ;;;
 
-(diag "INDENT")
-
-(is (indent 2 (format nil "1~%2~%3~%")) "  1
+(subtest "INDENT"
+  (is (indent 2 (format nil "1~%2~%3~%")) "  1
   2
   3
 " "basic case 1")
 
-(is (indent 2 (format nil "1~%2~%3")) "  1
+  (is (indent 2 (format nil "1~%2~%3")) "  1
   2
   3
-" "basic case 2")
+" "basic case 2"))
 
 
 (finalize)
